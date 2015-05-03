85cf60386b3b9e3d619158169939da081a0e963a
beanstown106 <nbenis106@gmail.com>
2015-05-03 03:50:51 -0400

    [2/2] build: Switch to Pixel sounds
    [ anirudhgupta109 | AOSiP ]
     - Remove AllAudio calls so that we have only Pixel sounds
     - Do not set default ringtone, notification and alarm to missing files
    
    Change-Id: I11528ac9c838ebef14c889f13f34dda458b2f7a4
    Signed-off-by: Anirudh Gupta <anirudhgupta109@gmail.com>

--- target/product/gsi_common.mk
+++ target/product/gsi_common.mk
@@ -20,14 +20,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/mainline_system.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/handheld_product.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_product.mk)
 
-# Default AOSP sounds
-$(call inherit-product-if-exists, frameworks/base/data/sounds/AllAudio.mk)
-
-# Additional settings used in all AOSP builds
-PRODUCT_PRODUCT_PROPERTIES += \
-    ro.config.ringtone=Ring_Synth_04.ogg \
-    ro.config.notification_sound=pixiedust.ogg \
-
 # The mainline checking whitelist, should be clean up
 PRODUCT_ARTIFACT_PATH_REQUIREMENT_WHITELIST += \
     system/app/messaging/messaging.apk \
