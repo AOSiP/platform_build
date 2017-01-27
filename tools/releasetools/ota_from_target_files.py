793a3590846be5cbf9376e5239d223377dad7756
HashBang <hashbang173@gmail.com>
2017-01-27 09:59:22 +0530

    Fix support for devices without ro.product.model
      * PS2- fixup indents
    
    Change-Id: I6fac98684918302b87cf38ebfaee7b3299898abf

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -492,7 +492,7 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
     script.Comment("Stage 3/3")
 
   # Dump fingerprints
-  script.Print("Target: %s" % target_fp)
+  #script.Print("Target: %s" % target_fp)
 
   script.Print("--------------------------------------------------")
   script.Print("    / AAAAAA  / OOOOOO   /ssssss /ii| /PPPPPP     ")
@@ -506,12 +506,15 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
   script.Print("                                                  ")
   script.Print("         Are you ready for the Illusion?          ")
   script.Print("--------------------------------------------------")
-  device = GetBuildProp("ro.product.device", OPTIONS.info_dict)
-  model = GetBuildProp("ro.product.model", OPTIONS.info_dict)
-  modver = GetBuildProp("ro.aosip.version", OPTIONS.info_dict)
   script.Print(" ")
-  script.Print("Device: %s (%s)"%(model, device))
-  script.Print("Version: %s"%(modver))
+  device = GetBuildProp("ro.aosip.device", OPTIONS.info_dict)
+  modver = GetBuildProp("ro.aosip.version", OPTIONS.info_dict)
+  if GetBuildProp("ro.product.model", OPTIONS.info_dict) is not None:
+    model = GetBuildProp("ro.product.model", OPTIONS.info_dict)
+    script.Print("Device: %s (%s)"%(model, device))
+  else:
+  	script.Print("Device: %s "%(device))
+  script.Print("Version: %s"%(modver));
 
   script.AppendExtra("ifelse(is_mounted(\"/system\"), unmount(\"/system\"));")
   device_specific.FullOTA_InstallBegin()
@@ -628,7 +631,8 @@ def GetBuildProp(prop, info_dict):
   try:
     return info_dict.get("build.prop", {})[prop]
   except KeyError:
-    raise common.ExternalError("couldn't find %s in build.prop" % (prop,))
+    print ("WARNING: could not find %s in build.prop" % (prop,))
+    return None
 
 
 def HandleDowngradeMetadata(metadata):
