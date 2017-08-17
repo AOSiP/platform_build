0901665780f246f57c74224af5e685a30ea0fb6e
Luca Stefani <luca020400@lineageos.org>
2017-08-17 19:49:57 +0200

    build: Include common AOSiP BoardConfig
    Change-Id: I2fa56d400ca2adf371dc3edb37e47619c7dd0a8e

--- core/config.mk
+++ core/config.mk
@@ -184,6 +184,7 @@ include $(BUILD_SYSTEM)/envsetup.mk
 FIND_LEAVES_EXCLUDES := $(addprefix --prune=, $(SCAN_EXCLUDE_DIRS) .repo .git)
 
 -include vendor/extra/BoardConfigExtra.mk
+-include vendor/aosip/config/BoardConfigAOSiP.mk
 
 # The build system exposes several variables for where to find the kernel
 # headers:
