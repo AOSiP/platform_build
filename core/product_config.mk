5c377ae88e9352f20332dda549027eda11ad47eb
Steve Kondik <shade@chemlab.org>
2017-08-22 23:16:15 +0200

    Fix fastpath code for AOSIP builds
    Change-Id: I3207dc1c8870548bc6536a9a401b712bce706c4e

--- core/product_config.mk
+++ core/product_config.mk
@@ -185,7 +185,7 @@ all_product_configs := $(call get-product-makefiles,\
     $(SRC_TARGET_DIR)/product/AndroidProducts.mk)
 else
  ifneq ($(AOSIP_BUILD),)
-    all_product_configs := device/*/$(AOSIP_BUILD)/aosip.mk
+    all_product_configs := $(shell ls device/*/$(AOSIP_BUILD)/aosip.mk)
   else
     # Read in all of the product definitions specified by the AndroidProducts.mk
     # files in the tree.
