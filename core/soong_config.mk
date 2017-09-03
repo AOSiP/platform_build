792dfd1e7c6fe537ee6c1c5cca8e2a9baf79c2f2
Nick Reuter <nreuter85@gmail.com>
2017-09-03 06:05:12 -0500

    soong: pass the qcom hal paths to soong
    Change-Id: Ie75e0a33172ac4008765b2d212c4de17dcb875e0

--- core/soong_config.mk
+++ core/soong_config.mk
@@ -72,7 +72,10 @@ $(SOONG_VARIABLES): FORCE aosip_soong
 	echo ''; \
 	echo '    "BtConfigIncludeDir": "$(BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR)",'; \
 	echo ''; \
-	echo '    "BoardUsesQTIHardware":  $(if $(BOARD_USES_QTI_HARDWARE),true,false)';  \
+	echo '    "BoardUsesQTIHardware":  $(if $(BOARD_USES_QTI_HARDWARE),true,false),'; \
+	echo '    "QTIAudioPath":  "$(call project-path-for,qcom-audio)",'; \
+	echo '    "QTIDisplayPath":  "$(call project-path-for,qcom-display)",'; \
+	echo '    "QTIMediaPath":  "$(call project-path-for,qcom-media)"';  \
 	echo '}') >> $(SOONG_VARIABLES_TMP); \
 	if ! cmp -s $(SOONG_VARIABLES_TMP) $(SOONG_VARIABLES); then \
 	  mv $(SOONG_VARIABLES_TMP) $(SOONG_VARIABLES); \
