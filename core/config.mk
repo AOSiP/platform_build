5cc3d607b8cb6a025c93e7e3cec93c8d9d750cea
Akhil Narang <akhilnarang.1999@gmail.com>
2018-08-08 01:24:08 +0530

    core: config: no sepolicy for now
    Change-Id: I72e0c86014ceeafe8c64d266a7dd8708c6d74cc1
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- core/config.mk
+++ core/config.mk
@@ -1097,7 +1097,7 @@ endif
 ifneq ($(AOSIP_BUILD),)
 ## We need to be sure the global selinux policies are included
 ## last, to avoid accidental resetting by device configs
-$(eval include device/aosip/sepolicy/common/sepolicy.mk)
+#$(eval include device/aosip/sepolicy/common/sepolicy.mk)
 
 # Include any vendor specific config.mk file
 -include vendor/*/build/core/config.mk
